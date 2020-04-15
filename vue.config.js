0535e3c58a4e5a1d4f411f492a4b146f08620d75
wuxiao <1291383707@qq.com>
2020-04-15 17:05:08 +0800

    添加 flexible.js以适配移动各端

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,21 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? '/fcnode/dist/'
-      : './'
+      : './',
+
+      css: {
+        modules: false,
+        extract: true,
+        sourceMap: false,
+        loaderOptions: {
+          css: {},
+          postcss: {
+            plugins: [
+              require('postcss-px2rem')({
+                remUnit: 75
+              })
+            ]
+          }
+        }
+      }
   }
